775ff96fea78f2bc0bb664d4c97c2c29dc715a47
kimhyunkwang <khkhy8712@gmail.com>
2021-01-03 22:18:21 +0900

    문제 해결 방법 제출

--- 2주차/2021-01-03/김현광/도도새의 업무일지.py
+++ 2주차/2021-01-03/김현광/도도새의 업무일지.py
@@ -4,11 +4,6 @@ a = []
 SUM = 0
 
 for i in range(n):
-    if i == 0:
-        a.append(b[i])
-    else:
-        a.append(b[i] * (i + 1) - SUM)
-
+    a.append(b[i] * (i + 1) - SUM)
     SUM += a[i]
-
     print(a[i], end=' ')
