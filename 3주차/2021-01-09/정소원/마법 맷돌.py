b9309fef2de7553cffb5dbacd0b7e379e00d1910
Sowon Jung <swj960515@gmail.com>
2021-01-12 23:07:44 +0900

    update: 2021-01-09 마법 맷돌 코드 및 md 수정

--- 3주차/2021-01-09/정소원/마법 맷돌.py
+++ 3주차/2021-01-09/정소원/마법 맷돌.py
@@ -2,10 +2,17 @@ s, t = [input() for i in range(2)]
 
 
 def solution(s, t):
-    for i in range(min(len(s), len(t))):
-        if s[i] != t[i]:
+    if len(s) >= len(t):
+        mx, mn = s, t
+    else:
+        mx, mn = t, s
+    j = 0
+    for i in range(len(mx)):
+        if mx[i] != mn[j]:
             return 0
+        j = (j+1)%len(mn)
     return 1
 
-
-print(solution(s, t))
+print(solution(s*2, t*2))
+# abaabaabaabac
+# aba
