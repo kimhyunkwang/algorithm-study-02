d31b48501a62ca0a1565a1b4f00541fdb36c79a1
Sowon Jung <swj960515@gmail.com>
2021-02-02 13:38:29 +0900

    update: 5주차 문제 1개 풀이 코드

--- 6주차/정소원/5주차_비타민 중독자.py
+++ 6주차/정소원/5주차_비타민 중독자.py
@@ -1,5 +1,5 @@
 n, m = map(int, input().split())
-invalid = [[i] for i in range(n)]
+invalid = [[i] for i in range(n)]  # 갈 수 없는 지점 저장
 
 for _ in range(m):
     a, b = map(int, input().split())
@@ -11,9 +11,10 @@ def dfs(v, l, ck, res):
     if l == 3:
         return res+1
     for i in range(n):
-        if i <= v:
+        if i <= v:  # 중복된 조합을 막기 위해 오름차순으로 숫자가 들어가도록 설정
             continue
         if i not in ck:
+            # 갈 수 없는 지점을 update하면서 조합 탐색
             res = dfs(i, l+1, list(set(ck+invalid[i])), res)
     return res
 
